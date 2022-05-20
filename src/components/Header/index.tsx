644f23f5571b97bcb417d2c2554106bcb12ae354
Vítor Marco <vitoradrianomarco@gmail.com>
2022-05-20 09:06:07 -0300

    Add SignIn button on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SignInButton } from "../SignInButton";
 
 import styles from "./styles.module.scss";
 
@@ -11,6 +12,8 @@ export const Header = () => {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
