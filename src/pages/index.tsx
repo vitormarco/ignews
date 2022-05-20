b83117f90995f95334828b862b87fc5e31241be0
Vítor Marco <vitoradrianomarco@gmail.com>
2022-05-20 00:24:59 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/home.module.scss";
+
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>World!</span>
+      </h1>
+    </>
+  );
+}
