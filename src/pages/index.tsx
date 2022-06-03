bd908f416418ddc4971348e0c8c50981b7481b14
Vítor Marco <vitoradrianomarco@gmail.com>
2022-06-03 13:35:39 -0300

    home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <div>
+          <Image
+            src="/images/avatar.svg"
+            alt="Girl coding"
+            width={500}
+            height={500}
+          />
+        </div>
+      </main>
     </>
   );
 }
