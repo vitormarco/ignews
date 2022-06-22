535165b4c3f87a19e131ca2eb01e49bf6a8d4c32
Vítor Marco <vitoradrianomarco@gmail.com>
2022-06-22 08:45:51 -0300

    Checkout Session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -5,6 +6,15 @@ type SubscribeButtonProps = {
 };
 
 const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  };
+
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe Now
