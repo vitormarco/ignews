a8d2bf5d1620d40d4431731adefb0b248585eae5
Vítor Marco <vitoradrianomarco@gmail.com>
2022-06-23 12:26:09 -0300

    Create user

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
 import { signIn, useSession } from "next-auth/react";
+
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -8,15 +12,31 @@ type SubscribeButtonProps = {
 const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const { data: session } = useSession();
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn("github");
       return;
     }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   };
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
